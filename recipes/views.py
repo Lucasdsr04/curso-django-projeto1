d03a554e24a493652b073b66980e07df958ffdd7
Lucas dos Santos <sl.solucas@gmail.com>
2023-06-27 11:32:55 -0300

    using real model recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
-    context = {'recipes': [make_recipe() for _ in range(10)]}
+    recipes = Recipe.objects.all().order_by('-id')
+    context = {'recipes': recipes}
+    return render(request, 'recipes/pages/home.html', context=context)
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    context = {'recipes': recipes}
     return render(request, 'recipes/pages/home.html', context=context)
 
 
